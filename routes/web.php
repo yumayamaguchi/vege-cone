6f773e85abaeb5c5100d1b50304f6ee54d255401
yumayamaguchi <80366714+yumayamaguchi@users.noreply.github.com>
2021-06-18 18:23:29 +0900

    commit

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', 'App\Http\Controllers\ArticleController@index');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
